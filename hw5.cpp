242091e71d21b22f4feae867189687427a894be7
ccoulton <ccoulton@nevada.unr.edu>
2014-10-31 22:23:25 -0700

    corrected the solution from top down to bottom up check the that the previous solution is most optimal, takes in how many steps

--- hw5.cpp
+++ hw5.cpp
@@ -1,64 +1,80 @@
-#include <stdio.h>
-#include <stack>
-#include <iostream>
-#include <fstream>
-
+#include <stack>    //stack objects
+#include <iostream> //cout printing
+#include <cstdlib> //c string to int conversion
+//struct to hold the values needed.
+struct Step
+    {
+    char choice;
+    int  lastvalue;
+    int  currtotal;
+    };
+    
 using namespace std;
-void plan(int index, int input[][4], stack<char>* output);
+void plan(int index, int max, int input[][4], stack<Step>* output);
 
 int main(int argc, char** argv)
 	{ 
 	//initialize total the matrix, and the decission stack
-	int total = 0;
 	int hi_low_mat [2][4] = {{10,  1, 10, 10},
-							 { 5, 50,  5,  1}};
-	stack<char> decissions;
+							 { 5, 50,  5, 15}};
+	//sets up the temp value fto set upf for the when the stack would be empty
+    Step current;
+    current.choice = '\0';
+    current.lastvalue = 0;
+    current.currtotal = 0;
+ 
+	stack<Step> decissions;
+	decissions.push(current);
+	//sets up the number of steps to do to 
 	int index = 0;
-	
+	int max = atoi(argv[1]);
 	//call plan to start recurssion
-	plan(index, hi_low_mat, &decissions);
-	
+	plan(index, max, hi_low_mat, &decissions);
+	current = decissions.top();
 	//loop to print/calculate what decissions where made
-	while(!decissions.empty())
-		{ //print
-		cout<<decissions.top()<<" ";
-		//if delay add 0 to total
-		if (decissions.top() == 'd')
-			total += 0;
-		//if low stress was choosen add it to total
-		else if (decissions.top() == 'l')
-			total += hi_low_mat[0][index];
-		//if high stress was choosen add it to total
-		else
-			total += hi_low_mat[1][index];
-		//incerment index and pop the top of the stack to get to next
-		index++;
+	//if not at the end the list loop though
+	while(current.choice != '\0')
+		{ //print values at steps
 		decissions.pop();
+		cout<<"optimal total @ step: ";
+		cout<<current.currtotal;
+		cout<<" choice @ step: "<<current.choice<<endl;
+		current = decissions.top();
 		}
-	//print total
-	cout<<total<<endl;
 	return 1;
 	}
 	
-void plan(int index, int input[][4], stack<char>* output)
+void plan(int index, int max, int input[][4], stack<Step>* output)
 	{
-	//if not at the end ->recurse
-	if ((output->empty())&&(index != (sizeof(input[0])/4)-1))
-		plan(index+1, input, output);
-	//check what values are there
-	//check if low is > than high
-	if (input[0][index] > input[1][index])
-		{ //make sure that the output is not empty segs otherwise
-		  //if last decission was high stress delay this week
-		if ((!output->empty())&&(output->top() == 'h'))
-				output->push('d');
-		  //else pick low since that is most profitable
-		else
-			output->push('l');
-		}
-		//if high is greater pick high
-	else if (input[0][index] < input[1][index])
-		output->push('h');
-	//return to where called 
-	return;
+	//allows to check optimal answer at a certain point
+	if (index == max) // if index is at the break point return
+	    return;
+	/*we need to check the solution so far, see if adding low to it is
+	greater than a the total minus the last step plus high, basicly 
+	if total + low > (total - stack->top) + high.  For our sample matrix
+	run to the first place optimal would be 10, however if run to second,
+	we would need to reverse one step and add high to get 50, */
+	Step curr;  //stack top
+	curr = output->top(); 
+	output->pop(); //move top
+	Step temp; // temp storeage 
+	if (curr.currtotal + input[0][index] > (curr.currtotal - curr.lastvalue)+input[1][index])
+	    { // if total + low value > total - last value + high value
+	    temp.lastvalue = input[0][index];
+	    temp.currtotal = curr.currtotal + temp.lastvalue;
+	    temp.choice = 'l';  //low picked
+	    }
+	else if (curr.currtotal+input[0][index]<(curr.currtotal - curr.lastvalue)+input[1][index])
+	    {
+	    curr.choice = 'd';  //set 
+	    curr.currtotal -= curr.lastvalue;
+	    curr.lastvalue = 0;
+	    temp.choice = 'h';
+	    temp.lastvalue = input[1][index];
+	    temp.currtotal = curr.currtotal + temp.lastvalue;
+	    }
+	//push previous and current.
+	output->push(curr);
+	output->push(temp);
+	plan(index+1, max, input, output);
 	}
